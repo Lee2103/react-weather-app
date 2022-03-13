afd4a4740ffaabda762182607e8432261688344b
Lee2103 <k.lee@windowslive.com>
2022-03-13 16:51:17 +0000

    function Weather added

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App(){
       <Weather defaultCity="London"/>
       <footer>
         This project was coded by {""} <a href="" target="_blank" rel="noopener norefferer">Ludmila Lee Kaplanova</a>{""}
-        and is open-sourced on {""}<a href="https://github.com/Lee2103/react-weather-app" target="_blank">GitHub</a>{""}
-        and hosted on {""}<a href="https://competent-mcnulty-28fbd0.netlify.app/" target="_blank">Netlify</a>
+        and is open-sourced on {""}<a href="https://github.com/Lee2103/react-weather-app" target="_blank" rel="norefferer noopener">GitHub</a>{""}
+        and hosted on {""}<a href="https://competent-mcnulty-28fbd0.netlify.app/" target="_blank" rel="norefferer noopener">Netlify</a>
 
       </footer>
     </div>
