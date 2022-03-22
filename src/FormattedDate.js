ec7a30d5c6ece06a5bf6f25613c843d42850f18c
Lee2103 <k.lee@windowslive.com>
2022-03-22 14:17:04 +0000

    Code update

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,3 +1,4 @@
+import React from "react";
 export default function FormattedDate(props) {
   let days = [
     "Sunday",
--- src/Weather.js
+++ src/Weather.js
@@ -54,7 +54,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-dark w-100"
               />
             </div>
           </div>
