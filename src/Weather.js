3e1cdc4056ce6db3099a22b8a10b1fa172538b16
Lee2103 <k.lee@windowslive.com>
2022-03-26 13:10:15 +0000

    Forecast function added

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -60,6 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates}/>
               </div>
     );
   } else {
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,34 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+export default function WeatherForecastDay (props){
+    function maxTemperature () {
+        let temperature= Math.round(props.data.temp.max);
+        return '${temperature}°';
+    }
+    function minTemperature (){
+        let temperature = Math.round(props.data.temp.min);
+        return '${temperature}°';
+    }
+    function day(){
+        let date= new Date(props.data.dt*1000);
+        let day=date.getDay();
+        let days=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        return days[day];
+    }
+    return (
+        <div>
+            <div className="WeatherForecast-day">
+                {day()}
+            </div>
+            <WeatherIcon size={36} code={props.data.weather[0].icon}/>
+            <div className="WeatherForecast-temperatures">
+                <span className="WeatherForecast-temperature-max">
+                    {maxTemperature}
+                </span>
+                <span className="Weather-forecast-temperature-min">
+                    {minTemperature}
+                </span>
+            </div>
+        </div>
+    );
+}
